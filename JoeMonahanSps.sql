a1c4b5837ba41ddb7e41deeb05f24564c6065f6d
jlm00007 <124469833+jlm00007@users.noreply.github.com>
2024-02-22 15:22:22 -0500

    Added a possible alternative for my sp

--- JoeMonahanSps.sql
+++ JoeMonahanSps.sql
@@ -29,3 +29,18 @@ end;
 go
 
 EXEC spPlayerStat @Name = 'Aaron Judge';
+
+
+/** EXTRA sp or POSSIBLE Alternative for the spPlayerStat (Not for submission)**/
+CREATE or ALter PROC spPlayerStatPos
+@Pos NVARCHAR(MAX) 
+
+AS
+BEGIN
+SELECT [Name], [Position], [HomeRuns], [Strikeouts] FROM Player as P
+Inner JOIN Stat as S on S.PID=P.PID
+where [Position] = @Pos;
+end;
+go
+
+EXEC spPlayerStatPos @Pos = 'Catcher';
