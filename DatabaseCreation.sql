89924364966312cc4df9a1a1726b423ea508de7f
jlm00007 <124469833+jlm00007@users.noreply.github.com>
2024-02-22 14:03:46 -0500

    Update DatabaseCreation.sql

--- DatabaseCreation.sql
+++ DatabaseCreation.sql
@@ -1,51 +1,27 @@
-create Database BaseballSQL
+create Database BaseballDB
 GO 
 
-USE [BaseballSQL]
+USE [BaseballDB]
 GO
 
-/****** Object:  Table [dbo].[Team]    Script Date: 2/16/2024 12:34:51 PM ******/
+/****** Object:  Table [dbo].[Player]    Script Date: 2/22/2024 2:01:21 PM ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TABLE [dbo].[Team](
-	[TID] [smallint] NOT NULL,
-	[Team Name] [nvarchar](max) NOT NULL,
-	[City] [nvarchar](max) NULL,
-	[State] [nvarchar](max) NULL,
-	[League] [nvarchar](max) NULL,
-	[Division] [nvarchar](max) NULL,
- CONSTRAINT [PK_Team] PRIMARY KEY CLUSTERED 
-(
-	[TID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-GO
-
-
-
-/** Creation of Player Table **/
 CREATE TABLE [dbo].[Player](
 	[PID] [smallint] NOT NULL,
 	[Name] [nvarchar](max) NOT NULL,
-	[Number] [nvarchar](max) NULL,
+	[Number] [smallint] NULL,
 	[Position] [nvarchar](max) NULL,
 	[TID] [smallint] NULL,
- CONSTRAINT [PK_Player] PRIMARY KEY CLUSTERED 
+ CONSTRAINT [PK__Player__C5775520D946B6C1] PRIMARY KEY CLUSTERED 
 (
 	[PID] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-GO
-
-ALTER TABLE [dbo].[Player]  WITH CHECK ADD  CONSTRAINT [FK_Player_Stat] FOREIGN KEY([PID])
-REFERENCES [dbo].[Stat] ([PID])
-GO
-
-ALTER TABLE [dbo].[Player] CHECK CONSTRAINT [FK_Player_Stat]
 GO
 
 ALTER TABLE [dbo].[Player]  WITH CHECK ADD  CONSTRAINT [FK_Player_Team] FOREIGN KEY([TID])
@@ -57,7 +33,7 @@ GO
 
 
 
-/****** Object:  Table [dbo].[Stat]    Script Date: 2/20/2024 12:16:18 PM ******/
+/****** Object:  Table [dbo].[Stat]    Script Date: 2/22/2024 2:01:30 PM ******/
 SET ANSI_NULLS ON
 GO
 
@@ -66,15 +42,39 @@ GO
 
 CREATE TABLE [dbo].[Stat](
 	[PID] [smallint] NOT NULL,
-	[Hits] [nchar](10) NULL,
-	[Strikeouts] [nchar](10) NULL,
-	[Home Runs] [nchar](10) NULL,
- CONSTRAINT [PK_Stat] PRIMARY KEY CLUSTERED 
-(
-	[PID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+	[Hits] [smallint] NULL,
+	[Strikeouts] [smallint] NULL,
+	[HomeRuns] [smallint] NULL
 ) ON [PRIMARY]
 GO
 
+ALTER TABLE [dbo].[Stat]  WITH CHECK ADD  CONSTRAINT [FK_Stat_Player] FOREIGN KEY([PID])
+REFERENCES [dbo].[Player] ([PID])
+GO
+
+ALTER TABLE [dbo].[Stat] CHECK CONSTRAINT [FK_Stat_Player]
+GO
+
+
+/****** Object:  Table [dbo].[Team]    Script Date: 2/22/2024 2:01:46 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Team](
+	[TID] [smallint] NOT NULL,
+	[TName] [nvarchar](max) NOT NULL,
+	[City] [nvarchar](max) NOT NULL,
+	[State] [nvarchar](max) NOT NULL,
+	[League] [nvarchar](max) NOT NULL,
+	[Division] [nvarchar](max) NOT NULL,
+ CONSTRAINT [PK__Team__C456D729A506F30E] PRIMARY KEY CLUSTERED 
+(
+	[TID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
 
 
